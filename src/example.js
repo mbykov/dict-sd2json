5ffede661fcb1ca975d153e04ef4a074dfd16dd8
Michael Bykov <m.bykov@gmail.com>
2020-11-13 17:46:58 +0300

    i

--- src/example.js
+++ src/example.js
@@ -19,8 +19,18 @@ if (!dictpath) {
   // '../../../DICTS/StarDict'
 }
 
+let respath
 dictpath = '/home/michael/DICTS/StarDict/EnRuEn/stardict-LingvoUniversalEnRu-2.4.2/LingvoUniversalEnRu.ifo'
-let respath = '/home/michael/DICTS/LingvoUniversalEnRu.json'
+respath = '/home/michael/DICTS/LingvoUniversalEnRu.json'
+
+dictpath = '/home/michael/DICTS/StarDict/DeRuDe/stardict-UniversalDeRu-2.4.2/UniversalDeRu.ifo'
+respath = '/home/michael/b/synchro.js/dicts/UniversalDeRu.json'
+
+dictpath = '/home/michael/DICTS/StarDict/FrRuFr/stardict-UniversalFrRu-2.4.2/UniversalFrRu.ifo'
+respath = '/home/michael/b/synchro.js/dicts/UniversalFrRu.json'
+
+dictpath ='/home/michael/DICTS/StarDict/ItRuIt/stardict-UniversalItRu-2.4.2/UniversalItRu.ifo'
+respath  = '/home/michael/b/synchro.js/dicts/UniversalItRu.json'
 
 let docs = []
 let chunk = []
@@ -30,7 +40,7 @@ sd2js(dictpath)
     log('DESCR', res.descr)
     log('DOCS', res.docs.length)
 
-    // fse.writeJsonSync(respath, res.docs, {spaces: 2})
+    fse.writeJsonSync(respath, res.docs, {spaces: 2})
 
     // let rdocs = res.docs.slice(100, 110)
     let rdocs = docs.filter(rdoc=> rdoc.refs)
